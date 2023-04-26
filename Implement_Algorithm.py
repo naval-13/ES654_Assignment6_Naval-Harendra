6fe02b5db84491a06e8da72dd9e623f6c7c1643a
naval-13 <jagginaval1312@gmail.com>
2023-04-26 20:32:37 +0530

    fianl

--- Implement_Algorithm.py
+++ Implement_Algorithm.py
@@ -52,7 +52,7 @@ class Linear_Regression():
         #   error = -2*X_batch.T.dot(y_batch-X_batch.dot(temp)) + 1*(temp.T).dot(temp)
         #   temp = temp - (0.1/len(y_batch))*error
         # else:
-        error = -2*X_batch.T.dot(y_batch-X_batch.dot(temp)) 
+        error = -2*X_batch.T.dot(y_batch-X_batch.dot(temp))/X.shape[0]
         temp = temp - ((lr)/batch_size)*error
 
         thetas = temp
@@ -61,6 +61,47 @@ class Linear_Regression():
     self.fit_intercept = thetas[0]
 
     pass
+
+   
+  def Normal_Gradient_Decent(self, X, y, n_iter, lr):
+    Xnew = [[1]]*len(X)
+    X = np.array(X)
+    y = np.array(y)
+    Xnew = np.concatenate((Xnew,X), axis=1)
+  #   print(Xnew.shape)
+    thetas = np.ones(X.shape[1]+1)
+    temp = np.ones(X.shape[1]+1)
+  #   print(thetas.shape)
+    for i in range(1,n_iter+1):
+        h = np.array(Xnew.dot(thetas))
+        error = -2*Xnew.T.dot(y-h)/X.shape[0]
+      #   print(error.shape, thetas.shape)
+        temp = temp - (lr*error)
+        thetas = temp
+    self.coef_ = thetas[1:]
+    self.fit_intercept = thetas[0]
+
+    pass
+
+  def Stochastic_Gradient_Descent(self, X, y, epochs, lr):
+    Xnew = [[1]]*len(X)
+    X = np.array(X)
+    y = np.array(y)
+    Xnew = np.concatenate((Xnew, X), axis = 1)
+    thetas = np.ones(X.shape[1]+1)
+    temp = np.ones(X.shape[1]+1)
+    for j in range(epochs):
+
+        for i in range(X.shape[0]):
+            # for j in range(Xnew.shape[0]):
+            h = np.array(Xnew[i].dot(thetas))
+            error = -2*Xnew[i].T.dot(y[i]-h)/X.shape[0]
+            temp = temp - lr*error
+            thetas = temp
+    self.coef_ = thetas[1:]
+    self.fit_intercept = thetas[0]
+
+    pass 
 
   def predict(self, X):
     # Funtion to run the LinearRegression on a test data point
--- app.py
+++ app.py
@@ -9,53 +9,54 @@ from Implement_Algorithm import *
 
 import streamlit as st
 
-st.title("Visualize the effect of gradient descent for various learning rate")
+st.title(" Visualize the effect of gradient descent ")
 
-#  streamlit run c:\Users\jaggi\Desktop\ML\Assignment_6_Naval-Harendra\app.py
+#  streamlit run c:\Users\jaggi\Desktop\ML\ES654_Assignment6_Naval-Harendra\app.py
 
 
-st.header("Provide Input parameters : ")
+st.header(" Provide Input parameters : ")
 
 # Default Values
 LearningRate = 0.002
 Epochs = 50
 
 # Change the values according to input given
-LearningRate = st.slider(' 1 )      Enter the value of Learning Rate?', 0.01, 0.999)
+LearningRate = st.slider(' 1 ) Enter the value of Learning Rate?', 0.01, 0.999)
 st.write("value of learning rate :  ", LearningRate)
 
 
-Epochs = st.slider(' 2 )      Enter the Number of Epochs?', 0, 1000)
+Epochs = st.slider(' 2 ) Enter the Number of Epochs?', 0, 1000)
 st.write("Number of Epochs :  ", Epochs)
 
 #select the algorithm
 option = st.selectbox( 'Select the algorithm you want to run',
-    ('batch Gradient Descent', 'Stochastic Gradient Descent', 'Gradient Descent'))
+    (   'batch Gradient Descent',
+        'Stochastic Gradient Descent', 
+        'Gradient Descent'
+    )
+    )
 
 st.write('selected Algorithm :', option)
 
+np.random.seed(42)
 
-
-# Data
-np.random.seed(45)
-N = 5000
-P = 1
-X = pd.DataFrame(np.random.randn(N, P))
-y = pd.Series(np.random.randn(N))
-
-# X = 3*np.random.rand(100,1)
-# y = 9 + 2*X+np.random.rand(100,1)
-
+N = 500
+slope = 2.3
+intercept = 10
+X = pd.DataFrame(np.random.uniform(0, 10, N), columns = ['X'])
+y = slope * X['X'] + intercept + np.random.randn(N)
+y=pd.Series(y)
 
 LR = Linear_Regression(fit_intercept=True)
+
 if(option == "batch Gradient Descent" ):
     batchsize = 20
     LR.compute_Batch_Gradient_Decent(X,batchsize,y, Epochs, LearningRate)
 elif(option == 'Stochastic Gradient Descent'):
-    x = 20
+    LR.Stochastic_Gradient_Descent(X,y,Epochs,LearningRate)
 else :
-    x = 30
-
+    LR.Normal_Gradient_Decent(X,y,Epochs,LearningRate)
+    
 
 # print(LR.coef_)
 # print(LR.fit_intercept)
@@ -68,28 +69,13 @@ st.write('MAE: ', mae(y_predicted, y))
 st.write("---------------------------")
 
 
-# # Create a scatter plot of the testing set with the two features on the x-axis and the target variable on the y-axis
-# fig, ax = plt.subplots()
-# ax.scatter(X[0], X[1], c=y, alpha=0.7)
-
-# # Add two lines to the scatter plot, one for y_true and one for y_predicted  label='y_predicted' , label='y_true'
-# ax.plot(X[0], X[1], y_predicted, c='r',label='y_predicted')
-# ax.plot(X[0], X[1], y_true, c='g', label='y_true' )
-
-# # Add a legend and axis labels
-# ax.legend(loc='upper right')
-# ax.set_xlabel('Feature 1')
-# ax.set_ylabel('Feature 2')
-# ax.set_title('Prediction V/s Ground Truth Plot')
-
-
 fig, ax = plt.subplots()
 ax.scatter(X, y, label='Data')
 ax.plot(X, y_predicted, color='green', label='y-predicted')
 
 
 ax.legend()
-ax.set_xlabel('Feature')
+ax.set_xlabel('X')
 ax.set_ylabel('Target')
 ax.set_title('Prediction V/s Truth Plot')
 
